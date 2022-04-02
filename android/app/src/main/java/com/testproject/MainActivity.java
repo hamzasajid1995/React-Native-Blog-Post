03496f374e37ffbb53712deaa8e763fd71fc8f3d
Hamza Sajid <hamzasajid424@gmail.com>
2022-04-02 16:34:57 +0500

    splash screen, logo

--- android/app/src/main/java/com/testproject/MainActivity.java
+++ android/app/src/main/java/com/testproject/MainActivity.java
@@ -4,15 +4,27 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
+import android.os.Bundle;
+ 
+import org.devio.rn.splashscreen.SplashScreen; // react-native-splash-screen
+
+
 public class MainActivity extends ReactActivity {
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+
   @Override
   protected String getMainComponentName() {
     return "TestProject";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this, true);  // react-native-splash-screen
+    super.onCreate(null);
   }
 
   /**
