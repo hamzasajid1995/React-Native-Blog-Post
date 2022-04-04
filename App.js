293b1463768c2a9e33ef411ff7b36dd36fdfbae9
Hamza Sajid <hamzasajid424@gmail.com>
2022-04-05 00:35:42 +0500

    redux

--- App.js
+++ App.js
@@ -7,11 +7,17 @@
  */
 
 import React, {useEffect} from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {View, Text} from 'react-native';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 import {NavigationContainer} from '@react-navigation/native';
 
 import SplashScreen from 'react-native-splash-screen';
+
+import {persistor, store} from './src/redux/store';
+import {Provider, useDispatch, useSelector} from 'react-redux';
+import {setPosts} from './src/redux/postSlice';
+import {PersistGate} from 'redux-persist/lib/integration/react';
+import Toast from 'react-native-root-toast';
 
 const App = () => {
   useEffect(() => {
@@ -20,9 +26,34 @@ const App = () => {
 
   return (
     <SafeAreaProvider>
-      <Text>haha</Text>
-      <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Posts />
+        </PersistGate>
+        {/* <NavigationContainer></NavigationContainer> */}
+      </Provider>
     </SafeAreaProvider>
   );
 };
+
+function Posts() {
+  const {posts = []} = useSelector(state => state.post);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(data => data.json())
+      .then(res => {
+        dispatch(setPosts(res));
+        Toast.show('Data Fetched');
+      })
+      .catch(console.log);
+  }, []);
+
+  return (
+    <SafeAreaView>
+      <Text>{posts.length}</Text>
+    </SafeAreaView>
+  );
+}
 export default App;
---
+++ src/redux/store.js
@@ -0,0 +1,35 @@
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import postReducer from './postSlice';
+const rootReducer = combineReducers({post: postReducer});
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage: AsyncStorage,
+  blacklist: ['post'],
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export let persistor = persistStore(store);
